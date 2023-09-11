5cf0722c42636580d88997ed1a4abe70a06a63b0
DuduViolante <dudu.violante@gmail.com>
2023-09-11 18:39:03 -0300

    img criada

--- src/routes/Cadastro/index.jsx
+++ src/routes/Cadastro/index.jsx
@@ -1,5 +1,21 @@
 export default function PaginaCadastro(){
     return (
-<></>
+<>
+<header>
+    <h1>Cadastro da bike</h1></header>
+
+<main>
+    <section></section>
+<button></button>
+<button></button>
+<button></button>
+<button></button>
+
+</main>
+
+
+
+
+</>
     )
 }
