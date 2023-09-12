a06a59bab8890e0b6928c535e1434b12678455df
davidbryan20 <davidbryan20@icloud.com>
2023-09-11 22:46:26 -0300

    Acrescentado informações no cadastro

--- src/routes/Cadastro/cadastro_pessoa.jsx
+++ src/routes/Cadastro/cadastro_pessoa.jsx
@@ -8,6 +8,9 @@ export default function PaginaCadastro(){
         <section className="Cadastro_Prop">
         <h2>Cadastre-se</h2>
         <input type="text" id="Name" >Digite seu Nome Completo</input>
+        <input type="text" id="cpf">Digite seu CPF</input>
+        <input type="text" id="rg">Digite seu RG</input>
+        <input type="text" id="endereco">Digite seu Endereço</input>
         <input type="email" id="email">Digite seu Email</input>
         <input type="password" id="senha">Digite sua Senha</input>
         <input type="email" id="conf_email">Confirme seu Email</input>
