6d7fca6aef8f3dcdf0624851bfde1e1e2cca03dd
davidbryan20 <davidbryan20@icloud.com>
2023-09-11 22:02:13 -0300

    feito o index do perfil do proprietário

--- src/routes/Perfil/index.jsx
+++ src/routes/Perfil/index.jsx
@@ -1,5 +1,17 @@
 export default function PaginaPerfil(){
     return (
-<></>
+<>
+    <h2>Olá, </h2>
+    <h2>Name</h2>    
+    <section className="PgnPerf">
+    <img src="./img/perfil.png"/>
+    <h2>Status do Seguro</h2>
+    <h1>Ajuda ?</h1>
+    <h2>Dados Pessoais</h2>
+    <h2>Detalhes do Seguro</h2>
+    <h2>Contrato</h2>
+    <h2>Termos de Uso</h2>
+    </section>
+</>
     )
 }
