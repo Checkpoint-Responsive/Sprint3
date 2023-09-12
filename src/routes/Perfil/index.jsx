4f715a35f48d1f0bd0711779e502d3ff4bef9dfe
davidbryan20 <davidbryan20@icloud.com>
2023-09-11 23:29:09 -0300

    Feito o index e o perfil

--- src/routes/Perfil/index.jsx
+++ src/routes/Perfil/index.jsx
@@ -1,20 +1,28 @@
-import 'perfil.css'
-export default function PaginaPerfil(){
-    return (
-<>
-    <h2>Olá, </h2>
-    <h2>Name</h2>    
-    <section className="PgnPerf">
-    <img src="./img/perfil.png"/>
-    <h2>Status do Seguro</h2>
-    <h1>Ajuda ?</h1>
-    <h2>Dados Pessoais</h2>
-    <h2>Detalhes do Seguro</h2>
-    <h2>Contrato</h2>
-    <h2>Termos de Uso</h2>
-    <img src="./img/sair.png"/>
-    <h2>Sair</h2>
-    </section>
-</>
-    )
+import './perfil.css'; // Importe seu arquivo CSS
+
+export default function PaginaPerfil() {
+  return (
+    <>
+      <h2 className="ola">Olá, Nome</h2>
+      <section className="PgnPerf">
+        <img
+          className="imagem-perfil"
+          src="./img/perfil.png"
+          alt="Imagem de perfil"
+        />
+        <h2 className="status-seguro">Status do Seguro</h2>
+        <h1 className="ajuda">Ajuda ?</h1>
+        <h2 className="dados-pessoais">Dados Pessoais</h2>
+        <h2 className="detalhes-seguro">Detalhes do Seguro</h2>
+        <h2 className="contato">Contato</h2>
+        <h2 className="termos-uso">Termos de Uso</h2>
+        <img
+          className="imagem-sair"
+          src="./img/sair.png"
+          alt="Imagem de sair"
+        />
+        <h2 className="texto-sair">Sair</h2>
+      </section>
+    </>
+  );
 }
