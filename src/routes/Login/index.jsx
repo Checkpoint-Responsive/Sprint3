460427d15330ea951d2dcee3a92229681685c482
Pedro Gomes <pedro.gomesf2004@gmail.com>
2023-09-11 21:42:07 -0300

    imagem da camera adicionada

--- src/routes/Login/index.jsx
+++ src/routes/Login/index.jsx
@@ -1,5 +1,29 @@
+
 export default function PaginaLogin(){
     return (
-<></>
+<>
+<div className="AndroidSmall9" style={{width: 360, height: 640, position: 'relative', background: '#7BC0FF', border: '0.50px black solid'}}>
+
+  <div className="Rectangle20" style={{width: 180, height: 42, left: 87, top: 375, position: 'absolute', background: '#D9D9D9', borderRadius: 50, border: '0.50px black solid'}}></div>
+
+  <div className="Login" style={{width: 69, height: 41, left: 279, top: 42, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Caudex', fontWeight: '700', wordWrap: 'break-word'}}>Login</div>
+
+  <div className="Entrar" style={{width: 69, height: 41, left: 146, top: 383, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Caudex', fontWeight: '700', wordWrap: 'break-word'}}>Entrar</div>
+
+  <div className="Senha" style={{width: 69, height: 41, left: 38, top: 271, position: 'absolute', color: 'white', fontSize: 20, fontFamily: 'Caudex', fontWeight: '700', wordWrap: 'break-word'}}>Senha</div>
+
+  <div className="Username" style={{width: 140, height: 41, left: 37, top: 166, position: 'absolute', color: '#F4F1F1', fontSize: 20, fontFamily: 'Caudex', fontWeight: '700', wordWrap: 'break-word'}}>Username</div>
+
+  <div className="EsqueciMinhaSenha" style={{width: 101, height: 14, left: 127, top: 420, position: 'absolute', color: '#F4F1F1', fontSize: 10, fontFamily: 'Caudex', fontWeight: '700', wordWrap: 'break-word'}}>Esqueci minha senha</div>
+
+  <div className="CadastreSe" style={{width: 101, height: 14, left: 135, top: 526, position: 'absolute', color: '#F4F1F1', fontSize: 15, fontFamily: 'Caudex', fontWeight: '700', wordWrap: 'break-word'}}>Cadastre-se</div>
+
+  <div className="Line1" style={{width: 281, height: 0, left: 37, top: 207, position: 'absolute', border: '0.50px white solid'}}></div>
+
+  <div className="Line2" style={{width: 281, height: 0, left: 37, top: 306, position: 'absolute', border: '0.50px white solid'}}></div>
+  
+  <img className="Image2" style={{width: 208, height: 35, left: 76, top: 578, position: 'absolute'}} src="https://via.placeholder.com/208x35" />
+</div>
+</>
     )
 }
