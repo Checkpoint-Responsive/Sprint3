c315544a8ba6024fc6093b7eaee936c8544276b4
Pedro Gomes <pedro.gomesf2004@gmail.com>
2023-09-11 22:00:27 -0300

    feito html login.

--- src/routes/Login/index.jsx
+++ src/routes/Login/index.jsx
@@ -2,28 +2,51 @@
 export default function PaginaLogin(){
     return (
 <>
-<div className="AndroidSmall9" style={{width: 360, height: 640, position: 'relative', background: '#7BC0FF', border: '0.50px black solid'}}>
+<div className="Desktop2" 
+style={{width: 1440, height: 1024, position: 'relative', background: '#7BC0FF'}}>
 
-  <div className="Rectangle20" style={{width: 180, height: 42, left: 87, top: 375, position: 'absolute', background: '#D9D9D9', borderRadius: 50, border: '0.50px black solid'}}></div>
+  <div className="Rectangle20" 
+  style={{width: 410, height: 76, left: 515, top: 600, position: 'absolute', background: 'white', borderRadius: 50}}>
+  </div>
 
-  <div className="Login" style={{width: 69, height: 41, left: 279, top: 42, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Caudex', fontWeight: '700', wordWrap: 'break-word'}}>Login</div>
+  <div className="Entrar" 
+  style={{width: 136, height: 60, left: 655, top: 616, position: 'absolute', color: 'black', fontSize: 40, fontFamily: 'Caudex', fontWeight: '700', wordWrap: 'break-word'}}
+  >Entrar</div>
 
-  <div className="Entrar" style={{width: 69, height: 41, left: 146, top: 383, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Caudex', fontWeight: '700', wordWrap: 'break-word'}}>Entrar</div>
+  <div className="Senha" 
+  style={{width: 609, height: 61, left: 280, top: 422, position: 'absolute', color: 'white', fontSize: 40, fontFamily: 'Caudex', fontWeight: '700', wordWrap: 'break-word'}}
+  >Senha</div>
 
-  <div className="Senha" style={{width: 69, height: 41, left: 38, top: 271, position: 'absolute', color: 'white', fontSize: 20, fontFamily: 'Caudex', fontWeight: '700', wordWrap: 'break-word'}}>Senha</div>
+  <div className="Username" 
+  style={{width: 718, height: 60, left: 275, top: 244, position: 'absolute', color: '#F4F1F1', fontSize: 40, fontFamily: 'Caudex', fontWeight: '700', wordWrap: 'break-word'}}
+  >Username</div>
 
-  <div className="Username" style={{width: 140, height: 41, left: 37, top: 166, position: 'absolute', color: '#F4F1F1', fontSize: 20, fontFamily: 'Caudex', fontWeight: '700', wordWrap: 'break-word'}}>Username</div>
+  <div className="EsqueciMinhaSenha" 
+  style={{width: 301, height: 20, left: 570, top: 686, position: 'absolute', color: '#F4F1F1', fontSize: 30, fontFamily: 'Caudex', fontWeight: '700', wordWrap: 'break-word'}}
+  >Esqueci minha senha</div>
 
-  <div className="EsqueciMinhaSenha" style={{width: 101, height: 14, left: 127, top: 420, position: 'absolute', color: '#F4F1F1', fontSize: 10, fontFamily: 'Caudex', fontWeight: '700', wordWrap: 'break-word'}}>Esqueci minha senha</div>
+  <div className="CadastreSe" 
+  style={{width: 195, height: 21, left: 623, top: 826, position: 'absolute', color: '#F4F1F1', fontSize: 35, fontFamily: 'Caudex', fontWeight: '700', wordWrap: 'break-word'}}
+  >Cadastre-se</div>
 
-  <div className="CadastreSe" style={{width: 101, height: 14, left: 135, top: 526, position: 'absolute', color: '#F4F1F1', fontSize: 15, fontFamily: 'Caudex', fontWeight: '700', wordWrap: 'break-word'}}>Cadastre-se</div>
+  <div className="Line1" 
+  style={{width: 892.52, height: 0, left: 275, top: 294.78, position: 'absolute', border: '0.50px white solid'}}>
+  </div>
 
-  <div className="Line1" style={{width: 281, height: 0, left: 37, top: 207, position: 'absolute', border: '0.50px white solid'}}></div>
+  <div className="Line2" 
+  style={{width: 892.52, height: 0, left: 275, top: 476, position: 'absolute', border: '0.50px white solid'}}>
+  </div>
 
-  <div className="Line2" style={{width: 281, height: 0, left: 37, top: 306, position: 'absolute', border: '0.50px white solid'}}></div>
-  
-  <img className="Image2" style={{width: 208, height: 35, left: 76, top: 578, position: 'absolute'}} src="https://via.placeholder.com/208x35" />
+  <img className="Image2" 
+  style={{width: 606, height: 92, left: 420, top: 913, position: 'absolute'}} src="https://via.placeholder.com/606x92"/>
+
+  <img className="Image24" 
+  style={{width: 136, height: 136, left: 27, top: 33, position: 'absolute'}} src="https://via.placeholder.com/136x136" />
+
 </div>
 </>
     )
 }
+
+// npm react-router-dom 2
+// npm run dev 3
