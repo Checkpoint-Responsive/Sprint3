6dd711f871c2a4190422996575ce2ebc2bcce4c7
davidbryan20 <davidbryan20@icloud.com>
2023-09-11 23:35:00 -0300

    colocado a rota na main

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import PaginaCadastro from './routes/Cadastro/index.jsx'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-
-
-
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import PaginaCadastro from './routes/Cadastro/index.jsx';
+import PaginaCadastroPessoa from './routes/Cadastro/index.jsx'; // Corrigido o nome do componente
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([
-{
-path: "/", 
-element: <App/>,
-errorElement: <Error/>,
-children:[
-{
-  path: "/pagina-cadastro",
-  element:<PaginaCadastro/>
-
-}
-]
-}
-
-])
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/pagina-cadastro',
+        element: <PaginaCadastro />
+      }, // Adicionado uma vírgula para separar as rotas
+      {
+        path: '/pagina-cadastropessoa',
+        element: <PaginaCadastroPessoa />
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
