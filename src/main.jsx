ae9616f0f2df43b814cd55bccdae9d53669f4193
davidbryan20 <davidbryan20@icloud.com>
2023-09-12 21:24:49 -0300

    Atualizado as routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import PaginaCadastro from './routes/Cadastro/index.jsx'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Error from './routes/Error/index.jsx'
-import PaginaLogin from './routes/Login/index.jsx'
-
-
-
-
-
-
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import PaginaCadastro from './routes/Cadastro/index.jsx';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Error from './routes/Error/index.jsx';
+import PaginaLogin from './routes/Login/index.jsx';
 
 const router = createBrowserRouter([
-{
-path: "/", 
-element: <App/>,
-errorElement: <Error/>,
-children:[
-{
-  path: "/pagina-cadastro",
-  element:<PaginaCadastro/>
-
-
-},
-{
-  path: "/pagina-login",
-  element:<PaginaLogin/>
-}
-]
-}
-
-])
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/pagina-cadastro',
+        element: <PaginaCadastro />
+      },
+      {
+        path: '/pagina-login',
+        element: <PaginaLogin />
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
